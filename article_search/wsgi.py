50f4e56148222595f40f3490de6e00a170016992
Anoopsmohan <anoopmhn2009@gmail.com>
2016-04-21 21:47:47 +0530

    update wsgi file

--- article_search/wsgi.py
+++ article_search/wsgi.py
@@ -10,5 +10,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "article_search.settings")
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
